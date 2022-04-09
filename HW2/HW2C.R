6e084eedb7b4aaa6c71d77de3af684d463334de7
Jeff <71949957+wjeff1648@users.noreply.github.com>
2022-04-08 23:44:08 -0400

    Update

--- HW2/HW2C.R
+++ HW2/HW2C.R
@@ -3,14 +3,17 @@
 library(tidyverse)
 library(gutenbergr)
 library(tidytext)
+library(reshape2)
+library(wordcloud)
 
 dickens_list <- read_csv('dickens_gutenberg.csv')
 mirror <- 'http://gutenberg.readingroo.ms/'
-book <- gutenberg_download(dickens_list$'Gutenberg ID'[1], mirror = mirror)
+book <- gutenberg_download(dickens_list$`Gutenberg ID`[1],
+                           mirror = mirror) %>% slice(dickens_list$start[1]:n())
 
 for(i in 2:nrow(dickens_list)) {
   book <- book %>% add_row(gutenberg_download(dickens_list$`Gutenberg ID`[i],
-                                              mirror = mirror))
+                                              mirror = mirror) %>% slice(dickens_list$start[i]:n()))
 }
 
 tidy_dickens <- book %>% unnest_tokens(word, text) %>%
@@ -56,6 +59,18 @@ tidy_dickens_afinn %>% group_by(Name) %>%
 
 tidy_dickens_afinn %>% ggplot(aes(Name, value)) + geom_violin()
 
+new_ct <- tidy_dickens_afinn %>% group_by(Name) %>%
+  count(sentiment) %>% pivot_wider(names_from = sentiment, values_from = n) %>%
+  column_to_rownames(var = "Name")
+
+
+chSq2 <- chisq.test(new_ct)
+chSq2
+
+chSq2$observed
+chSq2$expected
+chSq2$stdres
+
 data_for_acast <- tidy_dickens_afinn %>% 
   count(word, sentiment, sort = TRUE)
 
@@ -64,3 +79,4 @@ acasted <- data_for_acast %>% acast(word ~ sentiment, value.var = "n",
 
 acasted %>% comparison.cloud(colors = c("firebrick4", "darkolivegreen4"),
                              max.words = 100)
+
