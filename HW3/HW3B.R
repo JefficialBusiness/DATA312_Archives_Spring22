8f02241a802a1b11936c6f62bc30ac05a75ad73a
Jeff <71949957+JefficialBusiness@users.noreply.github.com>
2022-04-09 21:26:02 -0400

    Update

--- HW3/HW3B.R
+++ HW3/HW3B.R
@@ -142,55 +142,53 @@ edwin_improv_harmony %>% select(pitch) %>%
   mutate(keycheck = map(pitch, ~is_diatonic(.x, key = 'c'))) %>%
   count(keycheck == TRUE)
 
-# Attempting 
-ei <- edwin_improv_melody
-
-for (k in keys()) {
-  ei <- ei %>% 
-    mutate(!!k:=map(pitch, ~is_diatonic(.x, key = k)))
-  
-}
-
-ei <- ei %>% pivot_longer(cols = keys(), names_to = 'key')
-
-ei %>% group_by(key) %>%
-  filter(value == TRUE) %>%
-  count(value) %>%
-  ggplot(aes(reorder(key, desc(n)), n)) + geom_col()
 
 # SHIFTING TO MIDI FILES OF CHOICE
 # File 1: https://musescore.com/user/22574601/scores/6544221
 # File 2: https://bitmidi.com/queen-bohemian-rhapsody-mid
 
-# The first file is an opening from the first season of one of my favorite 
-# anime, The Quintessential Quintuplets.
+# The first file, "Gotoubun no Kimochi", is an opening from the first season of 
+# one of my favorite anime, The Quintessential Quintuplets.
 # The second file is a version of Queen's "Bohemian Rhapsody"
 
-bohemian <- read_midi('Queen - Bohemian Rhapsody.midi')
+bohemian <- read_midi('Queen - "Bohemian Rhapsody".midi')
 gotoubun <- read_midi('gotoubun_no_kimochi.midi')
 
-# Observations: Both "Bohemian Rhapsody" and "Gotoubun no Kimochi" contain
+# Observations: Both ""Bohemian Rhapsody"" and "Gotoubun no Kimochi" contain
 # a wide variety of events, several of them being indicative that both are
 # recordings, rather than composures of sheet music. Specifically, in addition
 # to a presence of events such as changes in time and key signatures, programs, 
 # tempo, and notes, there are several controller-related events as well.
 
-my_songs <- bind_rows(mutate(bach, name = 'bohemian'),
-                   mutate(edwin_improv, name = 'gotoubun'))
+# "Gotoubun no Kimochi" has several "MIDI port" events that further imply that
+# it was a recording from a device that communicated data through a MIDI port.
+# "Bohemian Rhapsody" does not have this. "Gotoubun no Kimochi" also has "Key
+# Signature" events that permit my understanding that this piece is intended
+# to be in E major. No such event is present in "Bohemian Rhapsody", perhaps
+# because it is not specified.
+
+my_songs <- bind_rows(mutate(bohemian, name = 'bohemian'),
+                   mutate(gotoubun, name = 'gotoubun'))
 
 # Note length distribution analysis
 my_songs %>% group_by(name) %>% ggplot(aes(x = name, y = length)) +
   geom_boxplot()
 
-# Observation: Gotoubun no Kimochi's note length distribution is rather 
-# condensed near the bottom, indicating that notes are typically short. The few
-# outliers are relatively short in comparison to Bohemian Rhapsody, where
-# there are clearly several longer notes, with several very long outliers, one
-# even holding out for a length of 7000. The distribution is quite skewed in
-# Gotoubun no Kimochi, with many of the outliers above the interquartile range,
-# though they are close together. Bohemian Rhapsody's distribution is extremely 
-# skewed in favor of notes longer than than the interquartile range, considering 
-# the very long outlier note.
+# Observation: Gotoubun no Kimochi's note length distribution seems rather even
+# with exactly 6 outliers. The IQR indicates that notes are generally of a
+# length beneath 500. The minimum and maximum lengths are generally of equal
+# distance from the 25th and 75th percentile on the IQR, respectively. The
+# highest outlier note is above a note length of 2000. The reason for this may
+# be because it is a rather fast-paced song, relatively. Upon listening to it
+# there are few evident long notes.
+
+# On the other hand, "Bohemian Rhapsody"'s note distribution is not as even. 
+# There is a wide variety of outliers, including a note with a length of nearly 
+# 6000. The IQR of note lengths is quite low, however. The positions of the 
+# outlier notes are not equal in distance, and are rather sporadic. The
+# distribution might be consistent with the inconsistent pace of the song. There
+# are "slower" moments and there are "faster" ones.
+
 
 my_songs_notes <- my_songs %>% filter(!is.na(pitch)) %>%
   mutate(as_music_df(pitch),
@@ -201,21 +199,45 @@ my_songs_notes %>% group_by(name) %>%
   ggplot(aes(x = freq, y = velocity, color = length)) + 
   geom_point() + facet_wrap(~name)
 
-# Observation:
+# Observation: It is not clear whether either piece has a melody, though some
+# distinction can be seen for "Bohemian Rhapsody". This is not the case at all 
+# for "Gotoubun no Kimochi", which has no velocity variation whatsoever. 
+# Observed also is the singular outlier note in this plot, with respect to
+# length. This can be seen as a result of coloring the plots to reflect note 
+# length.
 
-# Plotting note frequencies with respect to time
+
+# Plotting note frequency as a function of time
 my_songs_notes %>% filter(name=='gotoubun') %>% 
   ggplot(aes(xmin=time,xmax=end_time,y=freq,color=velocity)) +
   geom_linerange() +
   scale_y_log10()
 
-# Observation:
+my_songs_notes %>% filter(name=='bohemian') %>% 
+  ggplot(aes(xmin=time,xmax=end_time,y=freq,color=velocity)) +
+  geom_linerange() +
+  scale_y_log10()
+
+# Observation: With some effort, it is possible to separate harmony from
+# melody in Gotoubun no Kimochi, as it can now be seen a more definitive
+# distribution of frequency. The harmony and melody are much more difficult to
+# make out in "Bohemian Rhapsody" with many of the notes clustered together.
+
+# Upon listening to "Bohemian Rhapsody", there is no foundation that can be
+# made out. The range of frequency in this piece is not as vast.
+
+# "Gotoubun no Kimochi" has a foundation/harmony, or a series of "deeper" notes
+# that can be heard in the recording.
 
 # Checking Keys
-gotoubun %>% filter(event == 'Note On') %>% select(pitch) %>%
-  map(~is_diatonic(.x, key = 'e'))
+my_songs_notes %>% filter(name=='gotoubun') %>% 
+  mutate(keycheck = map(pitch, ~is_diatonic(.x, key = 'e_m'))) %>%
+  count(keycheck == TRUE)
 
-bohemian %>% filter(event == 'Note On') %>% select(pitch) %>%
-  map(~is_diatonic(.x, key = 'a'))
+# Percentage True: 1653 / (763 + 1653) = 0.68418874172 = roughly 68%
 
+my_songs_notes %>% filter(name=='bohemian') %>% 
+  mutate(keycheck = map(pitch, ~is_diatonic(.x, key = 'c_'))) %>%
+  count(keycheck == TRUE)
 
+# Percentage True: 2770 / (3155 + 2770) = 0.46751054852 = roughly 47%
