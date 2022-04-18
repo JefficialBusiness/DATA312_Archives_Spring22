8f770c9fc6935e361761af7936f1c9fdff4e4e15
Jeff <71949957+JefficialBusiness@users.noreply.github.com>
2022-04-18 09:54:44 -0400

    Update

---
+++ HW4/HW4B_jw0143a.R
@@ -0,0 +1,14 @@
+# Jeffrey Williams
+# Homework 4B
+# Dr. Robinson
+# DATA-312
+
+# Clearing Workspace / Loading libraries
+rm(list = ls())
+gc()
+
+library(tidyverse)
+library(e1071)
+
+data("spam", package = 'kernlab')
+
